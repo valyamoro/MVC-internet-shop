a65ac4abb0a1d40db419f9828f3be14fbfffac73
kutlumbek <Moralek228@gmail.com>
2023-09-19 21:56:11 +0300

    Добавил систему мониторинга сессий, и обезопасил их

--- src/models/logout_model.php
+++ src/models/logout_model.php
@@ -1,6 +1,13 @@
 <?php
+include __DIR__ . '/../../security/monitoring/session.php';
 session_start();
+
+logSessionEvent('User logged out: ' . $_SESSION['user']['name']);
+
+session_unset();
 session_destroy();
+
+session_write_close();
 
 header('Location: /');
 die;
